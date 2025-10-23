ee7715b513fcac52483316e1f7184607fbc5af69
SilentJungle399 <vardhan.harsh4041@gmail.com>
2025-10-23 11:56:43 +0530

    fixed phrasing in otp mails

--- src/app/signup/metadata.ts
+++ src/app/signup/metadata.ts
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Sign Up - Aero GenV",
-	description: "Create your Aero GenV account and join our RC aircraft community",
+	title: "Sign Up - Aeromodelling club",
+	description: "Create your Aeromodelling club account and join our RC aircraft community",
 };
